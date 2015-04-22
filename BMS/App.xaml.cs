86ce84c33d3fab1cdd5a51b73c9853619a0b7686
Quentin-Suchet <quentin.suchet@epitech.com>
2015-04-22 18:39:26 -0500

    On met tout mais ca va changer
    Oh fait j'ai mis la solution visual studio hein

---
+++ BMS/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BMS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            ConsoleManager.Show();
+            BmsBootstrapper bootStrapper = new BmsBootstrapper();
+            bootStrapper.Run();
+        }
+    }
+}
---
+++ BMS/BmsBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BMS
+{
+    internal class BmsBootstrapper : UnityBootstrapper
+    {
+        protected override void ConfigureContainer()
+        {
+            this.Container.RegisterType<IShellView, Shell>();
+
+            base.ConfigureContainer();
+        }
+        protected override DependencyObject CreateShell()
+        {
+            ShellPresenter presenter = Container.Resolve<ShellPresenter>();
+            IShellView view = presenter.View;
+            view.ShowView();
+            return view as DependencyObject;
+        }
+    }
+}
