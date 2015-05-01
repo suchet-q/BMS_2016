f35baf361ffa79fe851e7a1c06f70614b13791a8
Mathieu-Heuzey <mathieu.heuzey@gmail.com>
2015-05-01 00:22:19 -0700

    Base clean et fonctionelle

---
+++ BMS/Shell.xaml.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using System.Windows;
+
+namespace BMS
+{
+    /// <summary>
+    /// Interaction logic for Shell.xaml
+    /// </summary>
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BMSModule/BMSModule.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace BMSModule
+{
+    public class BMSModule : IModule
+    {
+        private readonly IRegionViewRegistry regionViewRegistry;
+
+        public BMSModule(IRegionViewRegistry registry)
+        {
+            this.regionViewRegistry = registry;
+        }
+
+        public void Initialize()
+        {
+            regionViewRegistry.RegisterViewWithRegion("MainRegion", typeof(View.BMSView));
+        }
+    }
+}
