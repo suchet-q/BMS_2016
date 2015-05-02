3599a3d84dcaad536fbd0b3aac930b0dabfbd7db
Quentin-Suchet <quentin.suchet@epitech.com>
2015-05-02 01:38:24 -0500

    Add de l'affichage de la console d'output (pour debug)

--- BMS/App.xaml.cs
+++ BMS/App.xaml.cs
@@ -10,6 +10,7 @@ namespace BMS
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            ConsoleManager.Show();
             base.OnStartup(e);
             Bootstrapper bootstrapper = new Bootstrapper();
             bootstrapper.Run();
