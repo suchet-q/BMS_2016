a88eb24281df02e66f5f995783eeef5f6b5e6988
Quentin-Suchet <quentin.suchet@epitech.com>
2015-05-01 22:40:12 -0500

    Create ViewModel bindé a la View + modif pour que ca marche

--- BMSModule/BMSModule.cs
+++ BMSModule/BMSModule.cs
@@ -8,17 +8,29 @@ namespace BMSModule
 {
     public class BMSModule : IModule
     {
+        private readonly IRegionManager _manager;
+        private readonly IUnityContainer _container;
 
-        private readonly IRegionViewRegistry regionViewRegistry;
+        //        private readonly IRegionViewRegistry regionViewRegistry;
 
-        public BMSModule(IRegionViewRegistry registry)
+        //       public BMSModule(IRegionViewRegistry registry)
+        public BMSModule(IUnityContainer container, IRegionManager manager)
         {
-            this.regionViewRegistry = registry;
+            _container = container;
+            _manager = manager;
+         // this.regionViewRegistry = registry;
         }
 
         public void Initialize()
         {
-            regionViewRegistry.RegisterViewWithRegion("MainRegion", typeof(View.BMSView));
+            //           regionViewRegistry.RegisterViewWithRegion("MainRegion", typeof(View.BMSView));
+            // On utilise pas la methode de base pour register la view dans la region car comme on decouvre les modules a la volé,
+            //il faut d'abord les register dans le container (ce qui est normalement fait quand on ne decouvre pas les modules a la volé par le bootstrapper)
+            //pour qu'il puisse faire les injections de dependance et que le ViewModel puisse etre "bindé" sur la view
+
+
+            var view  = _container.Resolve<View.BMSView>();
+            _manager.Regions["MainRegion"].Add(view);
         }
     }
 }
---
+++ BMSModule/ViewModel/BMSViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMSModule.ViewModel
+{
+    class BMSViewModel : BindableBase
+    {
+
+    }
+}
