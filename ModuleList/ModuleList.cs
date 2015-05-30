d3f89caab257116a02a9794e067ddcfb21b5f7ff
Naushi <juliette.vachin@epitech.eu>
2015-05-30 16:57:00 +0200

    hijack de binding?

--- ModuleList/ModuleList.cs
+++ ModuleList/ModuleList.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Practices.Prism.Modularity;
 using Microsoft.Practices.Prism.Regions;
 using Microsoft.Practices.Unity;
+using Microsoft.VisualBasic.FileIO;
 using System;
+using System.Collections.Generic;
 
 namespace ModuleList
 {
@@ -9,12 +11,33 @@ namespace ModuleList
     {
         private readonly IRegionManager _manager;
         private readonly IUnityContainer _container;
-//        private readonly IModuleCatalog _catalog;
+        private readonly IModuleCatalog _catalog;
+        private List<Stock> _listStock;
 
          public ModuleList(IUnityContainer container, IRegionManager manager)
         {
             this._container = container;
             this._manager = manager;
+            _catalog = new ModuleCatalog();
+            _listStock = new List<Stock>();
+            // Parsing CSV
+            // TODO Change file path
+            TextFieldParser parser = new TextFieldParser(@"C:\Users\Naushi\Desktop\input.csv");
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(";");
+            string[] fields = parser.ReadFields();
+            while (!parser.EndOfData)
+            {
+                //Process row
+                fields = parser.ReadFields();
+                _listStock.Add(new Stock(fields[0], fields[1], fields[2]));
+                //foreach (string field in fields)
+                //{
+                //    //TODO: Process field
+                //    Debug.WriteLine(field);
+                //}
+            }
+            parser.Close();
 //            this._catalog = catalog;
          // this.regionViewRegistry = registry;
         }
@@ -22,8 +45,11 @@ namespace ModuleList
         public void Initialize()
          {
              TransientLifetimeManager tlm = new TransientLifetimeManager();
+             _container.RegisterType<List<Stock>, List<Stock>>();
              _container.RegisterType(typeof(object), typeof(View.ModuleListView), "ModuleListView", tlm);
-             _container.RegisterType<ViewModel.IModuleListViewModel, ViewModel.ModuleListViewModel>();
+             _container.RegisterType<ViewModel.IModuleListViewModel, ViewModel.ModuleListViewModel>(new InjectionConstructor(_catalog, _listStock));
+
+//             _container.RegisterType<ViewModel.IModuleListViewModel, ViewModel.ModuleListViewModel>();
              //var view = _container.Resolve<View.ModuleListView>();
             // _manager.Regions["MainNavigationRegion"].Add(view);
 
