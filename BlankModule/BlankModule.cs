877bdce3881e4cc4d91f9dd8e85f38ae5dd2f3df
barral-v <valentin.barral@epitech.eu>
2015-06-04 11:11:39 +0200

    Blank module for templating

---
+++ BlankModule/BlankModule.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlankModule
+{
+    class BlankModule : IModule
+    {
+        private readonly IRegionManager _manager;
+        private readonly IUnityContainer _container;
+
+        public BlankModule(IUnityContainer container, IRegionManager manager)
+        {
+            this._manager = manager;
+            this._container = container;
+        }
+
+        public void Initialize()
+        {
+            TransientLifetimeManager tlm = new TransientLifetimeManager();
+            _container.RegisterType(typeof(object), typeof(View.BlankModuleView), "BlankModuleView", tlm);
+            _container.RegisterType<ViewModel.IBlankModuleViewModel, ViewModel.BlankModuleViewModel>(new InjectionConstructor());
+        }
+    }
+}
---
+++ BlankModule/ViewModel/BlankModuleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlankModule.ViewModel
+{
+    public class BlankModuleViewModel : BindableBase, IBlankModuleViewModel
+    {
+    }
+}
