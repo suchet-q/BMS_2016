a20bcb7098e3c00537579e4822e9253a8f1591cb
Quentin-Suchet <quentin.suchet@epitech.com>
2015-08-21 11:35:25 +0200

    va bien niquer

---
+++ BMS/ViewModel/ShellViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service;
+using Service.DataAccess;
+using System.Collections.ObjectModel;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace BMS.ViewModel
+{
+    class ShellViewModel : ViewModelBase
+    {
+        ObservableCollection<ViewModelBase> _viewModels;
+
+        ObservableCollection<ViewModelBase> _menuModule;
+
+        readonly IModuleRepository          _moduleRepository;
+
+        readonly IModuleCatalog             _catalog;
+        IUnityContainer                     _container;
+        IRegionManager                      _manager;
+
+        public ShellViewModel(IModuleCatalog catalog, IUnityContainer container, IRegionManager manager)
+        {
+            _catalog = catalog;
+            _container = container;
+            _manager = manager;
+            _moduleRepository = new ModuleRepository(_catalog);
+            MenuModule.Add(new MenuModuleViewModel(_moduleRepository, _manager));
+
+        }
+
+        public ObservableCollection<ViewModelBase> MenuModule
+        {
+            get
+            {
+                if (_viewModels == null)
+                {
+                    _viewModels = new ObservableCollection<ViewModelBase>();
+                }
+                return _viewModels;
+            }
+        }
+
+
+        public ObservableCollection<ViewModelBase> ViewModels
+        {
+            get
+            {
+                if (_viewModels == null)
+                {
+                    _viewModels = new ObservableCollection<ViewModelBase>();
+                }
+                return _viewModels;
+            }
+        }
+    }
+}
