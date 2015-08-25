86374bf2251cc93f4b209e69ce72bbf0b927046d
barral-v <valentin.barral@epitech.eu>
2015-08-25 12:33:25 +0200

    mise en place du mOrdersModule

--- OrdersManagerModule/OrdersManagerModule.cs
+++ OrdersManagerModule/OrdersManagerModule.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,25 @@ using System.Threading.Tasks;
 
 namespace OrdersManagerModule
 {
-    class OrdersManagerModule
+    public class OrdersManagerModule : IModule
     {
+        IUnityContainer _container;
+        IAPI            _api;
+
+        public OrdersManagerModule(IUnityContainer container, IAPI api)
+        {
+            _container = container;
+            _api = api;
+        }
+
+        public void Initialize()
+        {
+            var viewModel = new ViewModel.OrdersManagerModuleViewModel(_api);
+            var view = new View.OrdersManagerModuleView();
+            view.DataContext = viewModel;
+            System.Console.Error.WriteLine("Initialize");
+            TransientLifetimeManager tlm = new TransientLifetimeManager();
+            _container.RegisterInstance(typeof(object), "OrdersManagerModuleView", view);
+        }
     }
 }
--- OrdersManagerModule/ViewModel/OrdersManagerModuleViewModel.cs
+++ OrdersManagerModule/ViewModel/OrdersManagerModuleViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Service;
+using Service.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace OrdersManagerModule.ViewModel
 {
-    class OrdersManagerModuleViewModel
+    public class OrdersManagerModuleViewModel : ViewModelBase
     {
+        IAPI _api;
+
+         public OrdersManagerModuleViewModel(IAPI api)
+        {
+            _api = api;
+
+            IEnumerable<Order> listOrder = _api.Orm.ObjectQuery<Order>("select * from orders");
+
+        }
     }
 }
